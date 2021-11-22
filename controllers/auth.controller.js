ca19c3eb8f68c8c8d7851633a85739f21e93e479
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-22 09:55:31 -0600

    Token created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,15 @@
 const User = require('../models/User.model');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports.register = async function (req, res) {
 	const user = await User.create({ ...req.body });
-	res.status(StatusCodes.CREATED).json({ user });
+	const token = jwt.sign(
+		{ userId: user._id, name: user.name },
+		process.env.JWT_SECRET,
+		{ expiresIn: '30d' },
+	);
+	res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 module.exports.login = async function (req, res) {

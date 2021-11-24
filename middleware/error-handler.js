9a36d6d4da92d68d527820ff04892c98a84e7beb
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-24 09:57:06 -0600

    Custom errors added

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,27 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-	console.log(err);
-	if (err instanceof CustomAPIError) {
-		return res.status(err.statusCode).json({ msg: err.message });
+	console.log(`Error: >>> ${err.name}`);
+	let customError = {
+		// set default
+		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+		msg: err.message || 'Something went wrong try again later',
+	};
+	if (err.name === 'ValidationError') {
+		customError.msg = Object.values(err.errors)
+			.map(item => item.message)
+			.join(', ');
+		customError.statusCode = 400;
 	}
-	return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+	if (err.code && err.code === 11000) {
+		customError.msg = `Duplicate value entered for ${
+			Object.keys(err.keyValue)[0]
+		} field, please choose another value`;
+		customError.statusCode = 400;
+	}
+	// return res
+	// 	.status(StatusCodes.INTERNAL_SERVER_ERROR)
+	// 	.json({ msg: err.message });
+	return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
